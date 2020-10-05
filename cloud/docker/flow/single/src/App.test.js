d7a7f8b7026a82a83d48be9981a71c6e6b151cf2
buddhamagnet <buddhamagnet@gmail.com>
2020-10-05 13:23:22 +0100

    extract cloud to nimbus

--- cloud/docker/flow/single/src/App.test.js
+++
@@ -1,3 +0,0 @@
-test("tells the truth", () => {
-  expect(true).toBe(true);
-});
--- cloud/docker/simple/node/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (_req, res) => {
-  res.send("hello");
-});
-
-app.listen("7000");
