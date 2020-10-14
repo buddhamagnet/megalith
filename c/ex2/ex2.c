ee8b6285bba1d2a031c71368a732e997e962d964
Dave Goodchild <admin@daves-mbp.home>
2020-10-14 13:21:43 +0100

    c more

---
+++ c/ex2/ex2.c
@@ -0,0 +1,15 @@
+// include standard IO headers.
+#include <stdio.h>
+
+// main - program entrypoint.
+int main(int argc, char *argv[])
+{ 
+    // declare and assign integer variable.
+    int distance = 100;
+
+    // Print out distance.
+    printf("You are %d miles away.\n", distance);
+
+    // Return with non-zero exit code.
+    return 0;
+}
